8a4b951af62ec5b08f3d9076566c40d5455aa6ed
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-05 21:56:22 +0200

    drawing shit that needs fixing :(

--- MainUnityProject/Assets/Scripts/drawing.cs
+++ MainUnityProject/Assets/Scripts/drawing.cs
@@ -4,13 +4,17 @@ using Radishmouse;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class drawing : MonoBehaviour
 {
 
     [SerializeField] InputAction drawAction;
     [SerializeField] InputAction mousePositionAction;
+
     
+    [Header("no touchies, this only for debugging!!!!")]
+    [SerializeField] Transform prikParent;
     [SerializeField] RectTransform[] prikker = new RectTransform[8];
 
     private TrailRenderer trailRenderer;
@@ -27,6 +31,15 @@ public class drawing : MonoBehaviour
         trailRenderer.time = int.MaxValue;
         
         cam = Camera.main;
+
+        prikParent = GameObject.FindGameObjectWithTag("Prik Parent").transform;
+        int i = 0;
+        foreach (RectTransform t in prikParent.GetComponentsInChildren<RectTransform>())
+        {
+            if (t == prikParent) continue;
+            prikker[i] = t;
+            i++;
+        }
     }
     
     void Update()
@@ -37,6 +50,17 @@ public class drawing : MonoBehaviour
             trailRenderer.time = int.MaxValue;
             Vector2 v = mousePositionAction.ReadValue<Vector2>();
             transform.position = cam.ScreenToWorldPoint(new Vector3(v.x, v.y, 1));
+
+            foreach (RectTransform t in prikker)
+            {
+                Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(cam, t.position);
+                print(screenPos); //NEEDS FIXING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                if (v.x >= screenPos.x - 20 && v.x <= screenPos.x + 20 &&
+                    v.y >= screenPos.y - 20 && v.y <= screenPos.y + 20)
+                {
+                    t.gameObject.GetComponent<Image>().color = Color.cyan;
+                }
+            }
         }
         else
         {
