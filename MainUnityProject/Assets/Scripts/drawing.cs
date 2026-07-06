4b01dee0636e920a914013ab9b97a1062f40ff80
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-06 09:00:53 +0200

    mere prik

--- MainUnityProject/Assets/Scripts/drawing.cs
+++ MainUnityProject/Assets/Scripts/drawing.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Radishmouse;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -12,8 +13,9 @@ public class drawing : MonoBehaviour
     [SerializeField] InputAction drawAction;
     [SerializeField] InputAction mousePositionAction;
 
-    
-    [Header("no touchies, this only for debugging!!!!")]
+
+    [Header("no touchies, this only for debugging!!!!")] [SerializeField]
+    List<RectTransform> order = new List<RectTransform>();
     [SerializeField] Transform prikParent;
     [SerializeField] RectTransform[] prikker = new RectTransform[8];
 
@@ -38,8 +40,17 @@ public class drawing : MonoBehaviour
         {
             if (t == prikParent) continue;
             prikker[i] = t;
+            t.name = i.ToString();
             i++;
         }
+    }
+
+    void Stop()
+    {
+        trailRenderer.Clear();
+        trailRenderer.enabled = false;
+        
+        foreach(RectTransform t in prikParent) t.gameObject.GetComponent<Image>().color = Color.white;
     }
     
     void Update()
@@ -51,21 +62,25 @@ public class drawing : MonoBehaviour
             Vector2 v = mousePositionAction.ReadValue<Vector2>();
             transform.position = cam.ScreenToWorldPoint(new Vector3(v.x, v.y, 1));
 
+            int i = 0;
             foreach (RectTransform t in prikker)
             {
-                Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(cam, t.position);
-                print(screenPos); //NEEDS FIXING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(null, t.position);
+                bool hasBeenTouched = order.Contains(t);
                 if (v.x >= screenPos.x - 20 && v.x <= screenPos.x + 20 &&
-                    v.y >= screenPos.y - 20 && v.y <= screenPos.y + 20)
+                    v.y >= screenPos.y - 20 && v.y <= screenPos.y + 20 &&
+                    !hasBeenTouched)
                 {
+                    order.Add(t);
                     t.gameObject.GetComponent<Image>().color = Color.cyan;
+                    trailRenderer.AddPosition(cam.ScreenToWorldPoint(new Vector3(t.position.x, t.position.y, 1)));
                 }
+                i++;
             }
         }
         else
         {
-            trailRenderer.Clear();
-            trailRenderer.enabled = false;
+            Stop();
         }
         
     }
