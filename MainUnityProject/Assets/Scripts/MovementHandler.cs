58b5de733c6ad62324e001b89520525d2bb72c31
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-08 20:57:09 +0200

    fix

--- MainUnityProject/Assets/Scripts/MovementHandler.cs
+++ MainUnityProject/Assets/Scripts/MovementHandler.cs
@@ -18,6 +18,6 @@ public class MovementHandler : MonoBehaviour
     void Update()
     {
         Vector2 movement = movementAction.ReadValue<Vector2>() * (movementSpeed * Time.deltaTime);
-        rb.linearVelocity = transform.forward * movement.y;
+        rb.linearVelocity = transform.forward * movement.y +  transform.right * movement.x;
     }
 }
