55a8a729c4227685b8a1c02468e059fc3221b1d9
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-09 11:02:24 +0200

    blahblahllbahbhahb

--- MainUnityProject/Assets/Scripts/Enemy/Enemy.cs
+++ MainUnityProject/Assets/Scripts/Enemy/Enemy.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections;
-using System.Numerics;
 using Unity.VisualScripting;
-using UnityEditor.AdaptivePerformance.Editor;
 using UnityEngine;
 using UnityEngine.AI;
 
